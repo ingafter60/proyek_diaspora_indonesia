e4ff8a401377c09b791524c790ceaba229c0e3bd
ing <inyoman_gurnitha@yahoo.com>
2020-08-28 11:51:21 +0700

    3. Create app 'home' and register it to project

--- diaspora/settings.py
+++ diaspora/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # new
+    'src.home',
+    # end new
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
